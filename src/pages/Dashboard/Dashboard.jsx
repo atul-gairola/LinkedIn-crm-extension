ea35054b3d62ae94f692847292a5ff957a999cc7
atul-gairola <atulgairola08@gmail.com>
2021-06-06 13:08:54 +0530

    Add permissions

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 function Dashboard() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    chrome.runtime.sendMessage({ action: 'initialize' }, (response) => {
+      // if (response) {
+        console.log(response);
+        setLoading(false);
+      // }
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
 }
 
 export default Dashboard;
