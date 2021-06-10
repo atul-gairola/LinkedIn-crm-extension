4feafd3a99882b3d976a54c6d3f073cc0e445dfc
atul-gairola <atulgairola08@gmail.com>
2021-06-10 20:14:17 +0530

    Add styling to header

--- src/pages/Dashboard/Header.jsx
+++ src/pages/Dashboard/Header.jsx
@@ -1,36 +1,64 @@
 import React, { useState } from 'react';
-import { Avatar, Pane, Heading } from 'evergreen-ui';
+import { Avatar, Pane, Heading, Text } from 'evergreen-ui';
 
-import styles from './Header.css';
+import './Header.css';
+import logo from '../../assets/img/icon.svg';
 
 function Header({ user, retConnections }) {
   const { fullName, profilePicture, totalConnections } = user;
 
   return (
-    <header className={styles.header}>
+    <header>
       <Pane
         display="flex"
         padding={10}
+        paddingX={30}
         background="#fff"
-        borderBottom="1px solid #000"
-        boxShadow=""
-        borderRadius={3}
+        boxShadow="0px 4px 20px rgba(0,0,0,0.06)"
       >
         <Pane flex={1} alignItems="center" display="flex">
-          <Heading size={600}>LinkedIn CRM</Heading>
+          <img style={{ width: 40, marginRight: 20 }} src={logo} alt="logo" />
+          <Heading is="h3" fontWeight="bold">
+            LinkedIn CRM
+          </Heading>
         </Pane>
-        <Pane>
-          <div>
-            <p>Updated connections</p>
-            <p>
-              {retConnections} / {totalConnections}
-            </p>
-          </div>
+        <Pane
+          display="grid"
+          gridTemplateColumns="1fr auto"
+          gridGap={50}
+          alignItems="center"
+        >
+          <Pane
+            display="grid"
+            gridTemplateColumns="auto auto"
+            gridGap={30}
+            aligntItems="center"
+          >
+            <Pane>
+              <Text whiteSpace="nowrap" size={300}>
+                Last Sync
+              </Text>
+              <br />
+              <Text whiteSpace="nowrap" size={200} fontWeight="bold">
+                9 / Jun / 2021 5:45 PM
+              </Text>
+            </Pane>
+            <Pane>
+              <Text whiteSpace="nowrap" size={300}>
+                Updated connections
+              </Text>
+              <br />
+              <Text whiteSpace="nowrap" size={200} fontWeight="bold">
+                {retConnections} / {totalConnections}
+              </Text>
+            </Pane>
+          </Pane>
           <Avatar
             name={fullName}
             size={40}
             color="purple"
             src={profilePicture}
+            cursor="pointer"
           ></Avatar>
         </Pane>
       </Pane>
