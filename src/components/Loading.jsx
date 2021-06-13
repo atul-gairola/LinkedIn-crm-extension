33dbc1952c97d39f58f626936b05616333200b3c
atul-gairola <atulgairola08@gmail.com>
2021-06-13 17:46:15 +0530

    Update loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Text } from 'evergreen-ui';
 import icon from '../assets/img/icon.svg';
 import './Loading.css';
 
-function Loading() {
+function Loading({ type }) {
+  const [showText, setShowText] = useState(false);
+
+  useEffect(() => {
+    if (type === 'init') {
+      const timer = setTimeout(() => setShowText(true), 6000);
+      return () => clearTimeout(timer);
+    }
+  }, []);
+
   return (
     <div className="loadingContainer">
       <div className="loadingIcon">
         <img src={icon} alt="loading" />
       </div>
+      {showText && (
+        <Text maxWidth={500} color="muted" size={600} marginTop={50}>
+          This might take sometime. Please bear with us as we organize all your
+          connections.
+        </Text>
+      )}
     </div>
   );
 }
